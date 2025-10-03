de69c6e97c87247a300558006dda64ac0782275a
Liam Paull <paulll@iro.umontreal.ca>
2025-10-03 13:24:24 -0400

    small fix

--- launchers/default.sh
+++ launchers/default.sh
@@ -3,6 +3,6 @@
 source /environment.sh
 
 source /opt/ros/noetic/setup.bash
-source /code/catkin_ws/devel/setup.bash --extend
+source /code/devel/setup.bash --extend
 
 exec roslaunch encoder_pose encoder_pose_node.launch veh:=$VEHICLE_NAME
